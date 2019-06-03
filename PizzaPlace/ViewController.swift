17d77756aa1cbcdb92bbe6cb1ee765685c32eb16
Dan <bdylan998@gmail.com>
2019-06-03 14:02:44 +0300

    Initial Commit

---
+++ PizzaPlace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PizzaPlace
+//
+//  Created by dainis.putans on 03/06/2019.
+//  Copyright © 2019 dainis.putans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
